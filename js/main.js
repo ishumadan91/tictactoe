7155f3530fe25e24b9fd797a0d83032c1f98474c
Ishu <ishu@minewhat.com>
2015-06-28 12:33:32 +0530

    Update 2015-06-28T07:03:28.337Z

--- js/main.js
+++ js/main.js
@@ -44,7 +44,7 @@
 /* 0 */
 /***/ function(module, exports, __webpack_require__) {
 
-	eval("\"use strict\";\n\nvar _interopRequire = function (obj) { return obj && obj.__esModule ? obj[\"default\"] : obj; };\n\nvar React = _interopRequire(__webpack_require__(1));\n\nvar Router = _interopRequire(__webpack_require__(157));\n\nvar routes = _interopRequire(__webpack_require__(196));\n\nRouter.run(routes, function (Handler) {\n  return React.render(React.createElement(Handler, null), document.body);\n});\n\n/*****************\n ** WEBPACK FOOTER\n ** ./src/scripts/main.js\n ** module id = 0\n ** module chunks = 0\n **/\n//# sourceURL=webpack:///./src/scripts/main.js?");
+	eval("\"use strict\";\n\nvar _interopRequire = function (obj) { return obj && obj.__esModule ? obj[\"default\"] : obj; };\n\nvar React = _interopRequire(__webpack_require__(1));\n\nvar Router = _interopRequire(__webpack_require__(157));\n\nvar routes = _interopRequire(__webpack_require__(196));\n\nRouter.run(routes, Router.HistoryLocation, function (Handler) {\n  return React.render(React.createElement(Handler, null), document.body);\n});\n\n/*****************\n ** WEBPACK FOOTER\n ** ./src/scripts/main.js\n ** module id = 0\n ** module chunks = 0\n **/\n//# sourceURL=webpack:///./src/scripts/main.js?");
 
 /***/ },
 /* 1 */
