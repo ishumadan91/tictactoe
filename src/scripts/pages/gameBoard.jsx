aa0048ded8cde0c66c5506b5caf4a551f3d75203
Ishu <ishu@minewhat.com>
2015-07-06 10:50:59 +0530

    basic grid

--- src/scripts/pages/gameBoard.jsx
+++ src/scripts/pages/gameBoard.jsx
@@ -1,29 +1,48 @@
 import React from 'react';
 
+class Cell extends React.Component {
+    constructor(props){
+        super(props);
+    }
+    render() {
+        return (
+            <li>
+                <a onClick={this.onClick}>{this.props.item}</a>
+            </li>
+        );
+    }
+    onClick() {
+
+    }
+}
 class GameBoard extends React.Component {
   
-  constructor(props){
-    super(props);
-    this.state = {
-      items : [],
-      loading: false
-    };
-  }
+    constructor(props){
+        super(props);
+        this.state = {
+            cells : ['', '', '', '', '', '', '', '', '']
+        };
+    }
 
-  componentDidMount() {
-  }
+    componentDidMount() {
+    }
 
-  componentWillUnmount() {
-  }
+    componentWillUnmount() {
+    }
 
-  render() {
-
-    return (
-      <div className="game-board-wrap">
-      
-      </div>
-    );
-  }
+    render() {
+        var items = [];
+        for(var i in this.state.cells) {
+            items.push(<Cell key={i} item={this.state.cells[i]} index={i} />)
+        }
+        return (
+            <div className="game-board-wrap">
+                <ul className="cells-list clearfix">
+                    {items}
+                </ul>
+            </div>
+        );
+    }
 }
 
 export default GameBoard;
